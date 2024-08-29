8d1390fa601488ba1f0b5d28fc54d03efec24e39
Roki K <kiroutp@yandex.ru>
2024-08-29 11:33:02 +0300

    fix linter Button.test.tsx, ThemeSwitcher.tsx

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,8 @@
-import { classNames } from "shared/lib/classNames/classNames";
-import { Theme, useTheme } from "app/providers/themeProvider";
-import LightIcon from "shared/assets/icons/theme-light.svg";
-import DarkIcon from "shared/assets/icons/theme-dark.svg";
-import { Button, ButtonTheme } from "shared/ui/Button/Button";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Theme, useTheme } from 'app/providers/themeProvider';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -14,7 +14,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
     <Button
       theme={ButtonTheme.CLEAR}
       onClick={toggleTheme}
-      className={classNames("", {}, [className])}
+      className={classNames('', {}, [className])}
     >
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
     </Button>
