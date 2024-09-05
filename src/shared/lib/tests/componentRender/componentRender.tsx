7e5104f1fc285371155ebc4c9a4f30c297a2684e
Roki K <kiroutp@yandex.ru>
2024-09-05 18:21:36 +0300

    add redux store, add first entity

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -2,23 +2,31 @@ import { ReactNode } from 'react';
 import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
+import { DeepPartial } from '@reduxjs/toolkit';
 import { MemoryRouter } from 'react-router-dom';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
 export interface ComponentRenderOptions {
-  route?: string
+  route?: string,
+  initialState?: DeepPartial<StateSchema>
 }
 
 export function componentRender(
   component: ReactNode,
   options: ComponentRenderOptions = {},
 ) {
-  const { route = '/' } = options;
+  const {
+    route = '/',
+    initialState,
+  } = options;
 
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTests}>
-        {component}
-      </I18nextProvider>
-    </MemoryRouter>,
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTests}>
+          {component}
+        </I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>,
   );
 }
