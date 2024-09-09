1c0eeb9ffd511b707226bd9b9d25bdad870e8487
Roki K <kiroutp@yandex.ru>
2024-09-09 14:06:08 +0300

    fix Modal theme, body theme

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -68,7 +68,6 @@ export const Modal = (props: ModalProps) => {
   const mods: Record<string, boolean> = {
     [cls.opened]: isOpen,
     [cls.isClosing]: isClosing,
-    [cls[theme]]: true,
   };
 
   return (
